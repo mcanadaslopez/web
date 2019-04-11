8bfb4d7c9deff28ab44e831b81632deaf9bd5ef7
marcelo <marcelo.canadaslopez@gmail.com>
2019-04-11 22:45:39 +0200

    11.04.2019 Cambios en js y pag album.html

--- js/jumbotron.js
+++ js/jumbotron.js
@@ -1,16 +1,36 @@
 $(document).ready(function(){
-	$("body").fadeOut(800, function(){
-		$("body").fadeIn(2000);
-	});
+	/*$("body").fadeOut(800, function(){
+		$("body").fadeIn(2000); */
 	
+	$(".jumbotron > .container > .display-4").hover(function()
+	{
+		$(this).css({
+		'color': 'red',
+		'text-shadow': '3px 2px #F78181',
+		});
+	});
+
+	$(".jumbotron > .container > .display-4").mouseleave(function()
+	{
+		$(this).css({
+		'color': 'white',
+		'text-shadow': 'none',
+		});
+	});
 
 
 	
 	$(".container > .row > .col-md-4 > p:even").hover(function()
 	{
+		$(this).prev().css({
+			'color': '#BDBDBD',
+
+		});
 		$(this).css({
+			
 			'padding-left' : 10,
 			'border-left' : "solid 1px",
+			'border-left-color' : 'red',
 			'font-size': '1rem',
 
 		});
@@ -18,10 +38,15 @@ $(document).ready(function(){
 
 	$(".container > .row > .col-md-4 > p:even").mouseleave(function()
 	{
+		$(this).prev().css({
+			'color': 'white',
+		});
 		$(this).css({
+			
 			'padding-left' : 0,
 			'border-left' : "none",
 			'font-size': '0.90rem',
+
 
 
 		});
